9ff8c1da685f752f440f487d7a8f708dedc06a0d
Nick Prühs <dev@npruehs.de>
2020-12-09 22:28:37 +0100

    Disable version logging during tests.

--- src/main/java/de/opengamebackend/util/logging/LogApplicationVersion.java
+++ src/main/java/de/opengamebackend/util/logging/LogApplicationVersion.java
@@ -5,11 +5,13 @@ import de.opengamebackend.util.config.SpringApplicationConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 
 @Component
+@Profile("!test")
 public class LogApplicationVersion {
     private SpringApplicationConfig springApplicationConfig;
     private ApplicationConfig applicationConfig;
